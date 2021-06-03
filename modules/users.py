99a91f77089d3fbb70a6abd551067d55ae2b88a1
NurTasin <nmuatasin2005@gmail.com>
2021-06-03 21:51:19 +0600

    Updated username and API key dictionary

--- modules/users.py
+++ modules/users.py
@@ -2,8 +2,7 @@ from flask import jsonify
 
 def authenticate(api_key):
     authorized={
-        "Test":"test",
-        "0xfoodbabe":"foodbabe"
+        "admin":"password"
     }
     if api_key in authorized.keys():
         return authorized[api_key]
